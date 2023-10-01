18134ab01cf7b9b5a823897ac9e3b0aebd1443d9
oforosama <140859048+oforosama@users.noreply.github.com>
2023-10-01 03:48:58 -0700

    Worked on F1
    F1: reads the unit name and students' marks from a given text file. The user will provide the file name. The file contains the unit name and the list
    of students with their names, student IDs and marks for three assignments. The file also contains lines, which are comments, and your program
    should check to ignore them when reading the students' marks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Assignment-No2-Fundamentals-of-programming
+Please write the full Path of the txt file when asked for the name of the File.
+The format in which the txt should be written is also provider as a file named app.txt
+an Example is also given here.
+Unit 1
+John Doe, S12345, 85, 90, 78
+Alice Smith, S23456, 92, 88, 95
+Bob Johnson, S34567, 75, 81, 89
+// This is a comment line
+Eve Brown, S45678, 88, 79, 94
+jhon Snow, S45478, 88, 72, 54
+Charle, S34478, 45, 72, 54
+Bravo, S45434, 28, 72, 94
+Smith, S44578, 88, 92, 94
+Samantha, S42468, 98, 92, 94
---
+++ java/project/src/App.java
@@ -0,0 +1,135 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+// import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private String studentID;
+    private int[] marks = new int[3];
+    private int totalMark;
+
+    public Student(String name, String studentID, int[] marks) {
+        this.name = name;
+        this.studentID = studentID;
+        this.marks = marks;
+        calculateTotalMark();
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public String getStudentID() 
+    {
+        return studentID;
+    }
+
+    public int getTotalMark() 
+    {
+        return totalMark;
+    }
+
+    private void calculateTotalMark() 
+    {
+        for (int mark : marks) 
+        {
+            totalMark += mark;
+        }
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Name: " + name + ", Student ID: " + studentID + ", Marks: " +
+                marks[0] + ", " + marks[1] + ", " + marks[2] + ", Total Mark: " + totalMark;
+    }
+}
+
+public class App 
+{
+    private static List<Student> students = new ArrayList<>();
+
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        String fileName;
+
+        System.out.print("Enter the name of the text file: ");
+        fileName = scanner.nextLine();
+
+        readStudentDataFromFile(fileName);
+
+        while (true) 
+        {
+            System.out.println("\nMenu:");
+            System.out.println("1. List all students and their total marks");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice (1-2): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume newline
+
+            switch (choice) 
+            {
+                case 1:
+                    listAllStudents();
+                    break;
+                case 2:
+                    System.out.println("Exiting the program. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
+    }
+
+    private static void readStudentDataFromFile(String fileName) 
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) 
+        {
+            String line;
+            while ((line = reader.readLine()) != null) 
+            {
+                // Check for comment lines and ignore them
+                if (line.startsWith("//")) 
+                {
+                    continue;
+                }
+
+                // Split the line into parts: Name, Student ID, Assignment Marks
+                String[] parts = line.split(",");
+                if (parts.length == 5) 
+                {
+                    String name = parts[0].trim();
+                    String studentID = parts[1].trim();
+                    int[] marks = {
+                            Integer.parseInt(parts[2].trim()),
+                            Integer.parseInt(parts[3].trim()),
+                            Integer.parseInt(parts[4].trim())
+                    };
+                    Student student = new Student(name, studentID, marks);
+                    students.add(student);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading from the file: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static void listAllStudents() 
+    {
+        System.out.println("List of all students and their total marks:");
+        for (Student student : students) 
+        {
+            System.out.println(student);
+        }
+    }
+    
+}
